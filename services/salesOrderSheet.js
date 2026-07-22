11f2ffa87b1123c94082080f2574d2c0048abfa7
Arun <arunjamdagni2@gmail.com>
2026-07-22 12:08:17 +0530

    changes bhai

--- services/salesOrderSheet.js
+++ services/salesOrderSheet.js
@@ -94,20 +94,20 @@ export const appendSalesOrderToProductionProcess =
     }
 
 
-    const spreadsheetId =
-      getDatabaseByDivision(
-        division
-      );
+    // const spreadsheetId =
+    //   getDatabaseByDivision(
+    //     division
+    //   );
 
-    if (
-      !spreadsheetId
-    ) {
+    // if (
+    //   !spreadsheetId
+    // ) {
 
-      throw new Error(
-        `No database configured for division: ${division}`
-      );
+    //   throw new Error(
+    //     `No database configured for division: ${division}`
+    //   );
 
-    }
+    // }
 
 
     const authClient =
